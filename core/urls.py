e5ce97334fcb16eb6fddf7b7a6693b44c37a9358
salah uddin <mniloy695@gmail.com>
2026-05-23 15:01:37 +0600

    api endpoint organized for accoutns app

--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/accounts/', include("accounts.urls")),
+    path('api/accounts/auth/', include("accounts.urls")),
     # path('api/notifications/', include("notifications.urls")),
 ]
